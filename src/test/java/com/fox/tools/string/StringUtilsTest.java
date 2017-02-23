7401ae746a547460681bffc3a2c2ed17aba2d896
lcy362 <lcy362@163.com>
2017-02-23 15:01:21 +0800

    test

---
+++ src/test/java/com/fox/tools/string/StringUtilsTest.java
@@ -0,0 +1,13 @@
+package com.fox.tools.string;
+
+import org.junit.Test;
+
+/**
+ * Created by lcy on 2017/2/23.
+ */
+public class StringUtilsTest {
+    @Test
+    public void testGenerateRandomString() {
+        System.out.println(StringUtils.generateRandomString(100));
+    }
+}
