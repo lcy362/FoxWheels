ea70976a0c29fb9f10f96524b4f474b7d9076487
lcy362 <lcy362@163.com>
2017-03-09 15:56:52 +0800

    isequalcollection

---
+++ src/main/java/com/fox/tools/collection/CollectionTools.java
@@ -0,0 +1,46 @@
+package com.fox.tools.collection;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.Equator;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.util.Collection;
+
+/**
+ * Created by lcy on 2017/3/9.
+ */
+public class CollectionTools {
+
+    /**
+     * is all elements in two collections the same
+     * don't need to implenment equals() in T class
+     * @param l1
+     * @param l2
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean isEqualCollection(Collection<T> l1, Collection<T> l2) {
+        Equator<T> equator = new Equator<T>() {
+            @Override
+            public boolean equate(T o1, T o2) {
+                if (o1 == null && o2 == null) {
+                    return true;
+                }
+                if (o1 == null || o2 == null) {
+                    return false;
+                }
+                if (o1.getClass() != o2.getClass()) {
+                    return false;
+                }
+                return EqualsBuilder.reflectionEquals(o1, o2);
+            }
+
+            @Override
+            public int hash(T o) {
+                return HashCodeBuilder.reflectionHashCode(o);
+            }
+        };
+        return CollectionUtils.isEqualCollection(l1, l2, equator);
+    }
+}
